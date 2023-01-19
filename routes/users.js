d191f0f4ced83cf86ec91d226a2bed3462927c62
Hemanta <hemantadutta29@gmail.com>
2023-01-19 09:44:41 +0530

    Express Session Part 2

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
-var router = express.Router();
+var User = require("../models/user")
+const bodyParser = require('body-parser');
+var userRouter = express.Router();
+
+userRouter.use(bodyParser.json());
 
 /* GET users listing. */
-router.post('/signup', (req, res, next) => {
+userRouter.post('/signup', (req, res, next) => {
+  console.log(req.body);
   User.findOne({ username: req.body.username })
     .then((user) => {
       if (user != null) {
@@ -25,9 +30,9 @@ router.post('/signup', (req, res, next) => {
     .catch((err) => next(err));
 });
 
-router.post('/login', (req, res, next) => {
+userRouter.post('/login', (req, res, next) => {
+  if (!(req.session && req.session.user)) {
 
-  if (!req.session.user) {
     var authHeader = req.headers.authorization;
 
     if (!authHeader) {
@@ -40,20 +45,19 @@ router.post('/login', (req, res, next) => {
     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     var username = auth[0];
     var password = auth[1];
-
     User.findOne({ username: username })
       .then((user) => {
-        if (user === null) {
+        if (user._doc === null) {
           var err = new Error('User ' + username + ' does not exist!');
           err.status = 403;
           return next(err);
         }
-        else if (user.password !== password) {
+        else if (user._doc.password !== password) {
           var err = new Error('Your password is incorrect!');
           err.status = 403;
           return next(err);
         }
-        else if (user.username === username && user.password === password) {
+        else if (user._doc.username === username && user._doc.password === password) {
           req.session.user = 'authenticated';
           res.statusCode = 200;
           res.setHeader('Content-Type', 'text/plain');
@@ -69,7 +73,8 @@ router.post('/login', (req, res, next) => {
   }
 })
 
-router.get('/logout', (req, res) => {
+userRouter.get('/logout', (req, res) => {
+  console.log(req.session);
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
@@ -82,4 +87,4 @@ router.get('/logout', (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = userRouter;
