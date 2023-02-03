28dcebdad96d346f3d1e8184f2d04292488109a1
Hemanta <hemantadutta29@gmail.com>
2023-02-03 10:31:17 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '565345708872034',
+        clientSecret: '33939e83a964b6bc1afd5edadab5d1f8'
+    }
 }
